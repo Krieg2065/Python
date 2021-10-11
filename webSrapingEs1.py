c3fc5b7e352e3bb567095209281ca0463c6d6b34
Krieg <ismail.mohamed2065@gmail.com>
2021-10-11 16:40:34 +0200

    first attempt

--- webSrapingEs1.py
+++ webSrapingEs1.py
@@ -1,8 +1,27 @@
 # -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
 import requests
+import re
 
-html_txt = requests.get('https://www.wikihow.it/Creare-un-Blog-Personale').text
-soup = BeautifulSoup(html_txt,'lxml')
-passi = soup.find('li',class_ = 'whb')
-print(passi)
+html_txt = requests.get('https://www.wikihow.com/Breathe-Deeply#Video')
+# = BeautifulSoup(html_txt,'lxml')
+#passi = soup.find_all('li',class_ = 'b')
+#print(passi)
+#print(html_txt)
+#print(html_txt.text)
+soup = BeautifulSoup(html_txt.content,'html.parser')
+#print(soup)
+#passi = soup.find_all('div',class_ = 'step')
+#print(passi)
+#c = soup.find_all('b',class_ = 'whb')
+#print(c)
+c = soup.find_all("div",class_ =re.compile('mf-section-\d+'))
+#print(c)
+for i in c:
+    d = i.find_all('div', class_ = 'step')
+    #print(d)
+    name = i.find('span', class_ = 'mw-headline')
+    for z in d:
+        if z :
+            stepTitle = z.find('b',class_ = 'whb')
+            print(stepTitle)
